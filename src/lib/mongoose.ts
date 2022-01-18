2b2b42513d06aa3961f4ab48970f3eb48976d5e3
Mick Evans <51538065+codemastermick@users.noreply.github.com>
2022-01-18 13:57:29 -0500

    feat: add create method to mongoose

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -1,7 +1,8 @@
 import Logger from "./Logger";
 import CommonDatabaseService from "./common.database.service";
-import { connect, ConnectOptions, disconnect } from "mongoose";
+import { connect, ConnectOptions, disconnect, model } from "mongoose";
 import { dbURL } from "@config/database.config";
+import { User, userSchema } from "../models/user";
 
 export default class MongooseService extends CommonDatabaseService {
     logger: Logger;
@@ -11,15 +12,21 @@ export default class MongooseService extends CommonDatabaseService {
         super("MONGOOSE");
     }
 
-    connectToDatabase() {
-        connect(dbURL, this.options, () => {
-            this.logger.debug("Connected to MongoDB");
-        })
+    async connectToDatabase() {
+        await connect(dbURL, this.options);
+        this.logger.debug("Connected to MongoDB");
     }
 
-    disconnectFromDatabase() {
-        disconnect(() => {
-            this.logger.debug("Disconnected from MongoDB");
-        })
+    async disconnectFromDatabase() {
+        await disconnect();
+        this.logger.debug("Disconnected from MongoDB");
+    }
+
+    async create<T>(name: string, requestData: T): Promise<void> {
+        const instance = model<T>(name, userSchema);
+        const data = new instance(requestData);
+        await data.save();
+        this.logger.debug(`Saved new ${name}`);
+        await this.connectToDatabase();
     }
 }
