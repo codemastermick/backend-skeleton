7e55a8cf0201336014560752092a39386300f1f0
Mick Evans <51538065+codemastermick@users.noreply.github.com>
2022-01-18 16:58:13 -0500

    fix: move exception in auth to route level instead of module

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -9,26 +9,34 @@ export default class AuthRoutes extends CommonRoutesConfig {
         super(app, 'AuthRoutes');
     }
     configureRoutes(): express.Application {
-        if (!authenticationEnabled) {
-            throw new ApplicationException(StatusCodes.NOT_IMPLEMENTED, "Authentication is not enabled for this project. Please see src/configs/features.config.ts to enable a database.");
-        }
-
         this.app.route("/register").post((_req: express.Request, res: express.Response) => {
+            if (!authenticationEnabled) {
+                throw new ApplicationException(StatusCodes.NOT_IMPLEMENTED, "Authentication is not enabled for this project. Please see src/configs/features.config.ts to enable a database.");
+            }
             res.contentType("application/json");
             res.send({ 'message': 'Registration success!' });
         });
 
         this.app.route("/login").post((_req: express.Request, res: express.Response) => {
+            if (!authenticationEnabled) {
+                throw new ApplicationException(StatusCodes.NOT_IMPLEMENTED, "Authentication is not enabled for this project. Please see src/configs/features.config.ts to enable a database.");
+            }
             res.contentType("application/json");
             res.send({ 'message': 'Logged In!' });
         });
 
         this.app.route("/logout").get((_req: express.Request, res: express.Response) => {
+            if (!authenticationEnabled) {
+                throw new ApplicationException(StatusCodes.NOT_IMPLEMENTED, "Authentication is not enabled for this project. Please see src/configs/features.config.ts to enable a database.");
+            }
             res.contentType("application/json");
             res.send({ 'message': 'Logged Out!' });
         });
 
         this.app.route("/forgot-password").get((_req: express.Request, res: express.Response) => {
+            if (!authenticationEnabled) {
+                throw new ApplicationException(StatusCodes.NOT_IMPLEMENTED, "Authentication is not enabled for this project. Please see src/configs/features.config.ts to enable a database.");
+            }
             res.contentType("application/json");
             res.send({ 'message': 'Password reset emails not implemented yet!' });
         });
