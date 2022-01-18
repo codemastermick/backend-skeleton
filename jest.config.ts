6b8f579f4e2dabf3bdf4185e7aed10bf279a644e
Mick Evans <51538065+codemastermick@users.noreply.github.com>
2022-01-18 14:38:44 -0500

    chore: add aliases to jest config

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,9 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     moduleNameMapper: {
+        "@config/(.*)": "<rootDir>/src/configs/$1",
         "@lib/(.*)": "<rootDir>/src/lib/$1",
-        "@middleware/(.*)": "<rootDir>/src/middleware/$1"
+        "@middleware/(.*)": "<rootDir>/src/middleware/$1",
+        "@routes/(.*)": "<rootDir>/src/routes/$1",
     },
 };
