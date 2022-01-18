9091e1706ec8ccb9753dbf546211b68d30aabb53
Mick Evans <51538065+codemastermick@users.noreply.github.com>
2022-01-18 16:43:23 -0500

    feat: add prettier support

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: "none",
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  arrowParens: "always"
+};
