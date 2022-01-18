bd7ec399e35824aa49f1923c8e72ed303573ffed
Mick Evans <51538065+codemastermick@users.noreply.github.com>
2022-01-18 17:42:00 -0500

    chore: display name in log instead of two generic start messages

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,5 @@ import Server from "./app";
 const app = new Server();
 const logger = new Logger(process.env.APP_NAME.toUpperCase().replace(/ /g, '-'));
 
-logger.debug("Starting server....");
+logger.debug(`Starting ${process.env.APP_NAME}....`);
 app.start();
