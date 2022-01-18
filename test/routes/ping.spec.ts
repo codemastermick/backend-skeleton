c646f00d028e0d694f7ae09083b960e44f5549bb
Mick Evans <51538065+codemastermick@users.noreply.github.com>
2022-01-18 15:43:29 -0500

    test: add missing ping test

---
+++ test/routes/ping.spec.ts
@@ -0,0 +1,22 @@
+import request from "supertest";
+import express from "express";
+import PingRoutes from "../../src/routes/ping";
+
+let server: express.Express;
+let router: express.Router;
+
+beforeAll(async () => {
+    server = express();
+    router = express.Router();
+    router.use("/", new PingRoutes(server).configureRoutes())
+})
+
+describe('GET /ping', () => {
+    it('should return the correct response', () => {
+        return request(server).get("/ping").then(res => {
+            expect(res.statusCode).toBe(200)
+            expect(res.type).toBe('application/json')
+            expect(res.body.message).toBe("pong")
+        })
+    })
+})
