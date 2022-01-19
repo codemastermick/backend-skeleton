feefaca22009447414665ff9584fbe5febbc7a40
Mick Evans <51538065+codemastermick@users.noreply.github.com>
2022-01-19 11:00:36 -0500

    chore: schema adjustments

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -2,8 +2,10 @@ import { Schema } from "mongoose";
 
 export const userSchema = new Schema({
     uid: { type: String, required: true },
-    displayName: { type: String, required: true },
+    username: { type: String, required: true },
     password: { type: String, required: true },
+    displayName: { type: String, required: false },
     email: { type: String, required: false },
-    avatarUrl: { type: String, required: false }
+    avatarUrl: { type: String, required: false },
+    role: { type: String, required: true }
 })
