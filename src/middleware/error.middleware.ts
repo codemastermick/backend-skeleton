395e2556b7811f2f5f37d6f6425a9b7985a1fd40
Mick Evans <51538065+codemastermick@users.noreply.github.com>
2022-01-19 15:05:12 -0500

    chore: add eslint ignore for next function

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import HttpException from '../exceptions/HttpException';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const errorHandler = (error: HttpException, _request: Request, response: Response, _next: NextFunction) => {
     const status = error.status || StatusCodes.INTERNAL_SERVER_ERROR;
     const message = error.message || 'Something went wrong';
