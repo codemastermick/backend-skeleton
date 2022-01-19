d99792e20eee2ba867cd8f7eae4cff4725dd0619
Mick Evans <51538065+codemastermick@users.noreply.github.com>
2022-01-19 14:14:37 -0500

    feat: create middleware for checking jwt

---
+++ src/middleware/authentication.middleware.ts
@@ -0,0 +1,36 @@
+/* eslint-disable no-shadow */
+import Logger from '@lib/Logger';
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+
+declare module 'express-serve-static-core' {
+    interface Request {
+        user?: string | JwtPayload
+    }
+    interface Response {
+        user?: string | JwtPayload
+    }
+}
+const logger = new Logger("AUTH");
+
+const authenticateJWT = (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token, process.env.JWT_SIGNING_SECRET, (err: Error, user: JwtPayload) => {
+            if (err) {
+                logger.error(err.message);
+                return res.sendStatus(StatusCodes.FORBIDDEN);
+            }
+
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(StatusCodes.UNAUTHORIZED);
+    }
+};
+
+export default authenticateJWT;
