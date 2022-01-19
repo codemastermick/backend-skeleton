0cd7d3da74e83f74a98d9712d781404ca7d8d4b9
Mick Evans <51538065+codemastermick@users.noreply.github.com>
2022-01-19 14:42:22 -0500

    feat: add sample routes to show auth

--- src/routes/ping.ts
+++ src/routes/ping.ts
@@ -1,5 +1,15 @@
 import CommonRoutesConfig from '@lib/common.routes.config';
+import authenticateJWT from '@middleware/authentication.middleware';
+import UnauthorizedException from '../exceptions/Unauthorized';
 import express from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { JwtPayload } from 'jsonwebtoken';
+
+const sendPongResponse = (res: express.Response) => {
+    res.contentType("application/json");
+    res.status(StatusCodes.OK);
+    res.send({ 'message': 'pong' });
+}
 
 export default class PingRoutes extends CommonRoutesConfig {
     constructor(app: express.Application) {
@@ -10,9 +20,32 @@ export default class PingRoutes extends CommonRoutesConfig {
             // add logic here to run before running http verbs, like authentication
             next();
         }).get((_req: express.Request, res: express.Response) => {
-            res.contentType("application/json");
-            res.send({ 'message': 'pong' });
+            sendPongResponse(res);
         });
+
+        this.app.route("/auth-ping").all((req: express.Request, res: express.Response, next: express.NextFunction) => {
+            // add logic here to run before running http verbs, like authentication
+            authenticateJWT(req, res, next);
+            if (!req.user) {
+                throw new UnauthorizedException();
+            }
+            next();
+        }).get((req: express.Request, res: express.Response) => {
+            sendPongResponse(res);
+        });
+
+        this.app.route("/admin-ping").all((req: express.Request, res: express.Response, next: express.NextFunction) => {
+            // add logic here to run before running http verbs, like authentication
+            authenticateJWT(req, res, next);
+            next();
+        }).get((req: express.Request, res: express.Response) => {
+            const { role } = req.user as JwtPayload;
+            if (role !== 'admin') {
+                return res.sendStatus(StatusCodes.FORBIDDEN);
+            }
+            sendPongResponse(res);
+        });
+
         return this.app;
     }
 }
