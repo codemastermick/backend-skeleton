ad34dcd85f20171760e5d35c0fcfaa97faebbc75
Mick Evans <51538065+codemastermick@users.noreply.github.com>
2022-01-19 01:15:21 -0500

    chore: add missing payload data for user

--- tests/routes/crud.spec.ts
+++ tests/routes/crud.spec.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 import CrudRoutes from "../../src/routes/crud";
 import express from "express";
+import { StatusCodes } from "http-status-codes";
 
 let server: express.Express;
 let router: express.Router;
@@ -15,10 +16,13 @@ describe('POST /crud', () => {
     it('should return the correct response', () => {
         const payload = {
             "uid": "test",
-            "displayName": "Test User"
+            "displayName": "Test User",
+            "password": "secret",
+            "email": "demo@fakemail.net",
+            "avatarUrl": ""
         }
         return request(server).post("/crud").send(payload).then(res => {
-            expect(res.statusCode).toBe(200)
+            expect(res.statusCode).toBe(StatusCodes.CREATED)
             expect(res.type).toBe('application/json')
             expect(res.body.message).toBe("Create success!")
         })
