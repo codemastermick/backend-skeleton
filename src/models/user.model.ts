f3c91d1d0de2fdc4522fbbcadf81cfaed851e8b2
Mick Evans <51538065+codemastermick@users.noreply.github.com>
2022-01-21 12:42:49 -0500

    chore: remove uid and set optional props

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,9 +1,8 @@
 export interface User {
-  uid: string;
   username: string;
   password: string;
-  displayName: string;
-  email: string;
-  avatarUrl: string;
+  displayName?: string;
+  email?: string;
+  avatarUrl?: string;
   role: string;
 }
