86ae628cddd6acd5dc006d0665d512c4b1a05ad3
Mick Evans <51538065+codemastermick@users.noreply.github.com>
2022-01-21 12:43:22 -0500

    chore: remove uid from schema

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,7 +1,6 @@
 import { Schema } from 'mongoose';
 
 export const userSchema = new Schema({
-  uid: { type: String, required: true },
   username: { type: String, required: true },
   password: { type: String, required: true },
   displayName: { type: String, required: false },
